ed0ec2b0bfca5b0c3242d56f0594d9252fa7abbc
HeDiDiTieTie <y1163273458@163.com>
2023-03-27 08:44:49 +0800

    init git

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="container">
+		<u-subsection :list="list" :current="1"></u-subsection>
+		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
+		<text class="intro">详见：</text>
+		<uni-link :href="href" :text="href"></uni-link>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['未付款', '待评价', '已付款'],
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
